949a0a338aab111bd9975ef908f9e65db89bfef6
Mark Elliott <96553641+melliott7264@users.noreply.github.com>
2022-03-09 16:21:39 -0500

    Added initial password parameter input and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,45 @@ function writePassword() {
 
 }
 
+var generatePassword = function () {
+  
+// get the number of desired characters in the password
+var numChars = window.prompt("How many characters do you want in your password? (8 to 128)");
+//checking for Not a Number and numbers not between 8 and 128
+while (isNaN(numChars) || numChars < 8 || numChars > 128) {
+   numChars = window.prompt("Please only enter numbers between 8 and 128");
+}
+ 
+console.log(numChars + " characters");
+
+// does the user want lowercase characters
+var lowerChars = window.confirm("Do you want to include lowercase characters in your password?");
+if (lowerChars) {
+  console.log("User wants lowercase characters");
+} else {
+  console.log("User doesn't want lowercase characters");
+}
+
+// does the user want uppercase characters in their password
+var upperChars = window.confirm("Do you want to include uppercase characters in your password?");
+if (upperChars) {
+  console.log("User wants uppercase characters");
+} else {
+  console.log("User doesn't want uppercase characters");
+}
+
+// does the user want special characters in their password
+var specialChars = window.confirm("Do you want to include special characters in your password?")
+if (specialChars) {
+  console.log("User wants special characters");
+} else {
+  console.log("User doesn't want special characters");
+}
+
+  
+passwordString = "This is a placeholder for the final password";
+return passwordString;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
